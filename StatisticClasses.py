93e83896c1042a0bcb9ee5b9994599a1634a3f5a
Adam Kilbo <amkilbo@gmail.com>
2015-11-04 09:55:51 -0600

    Changed formatting, documented the function of each class.

--- StatisticClasses.py
+++ StatisticClasses.py
@@ -1,51 +1,87 @@
+##########################################################################################################################
+"""
+
+StatisticClasses.py, a file that holds class definitions for classes that collect statistics.
+
+JSONDictionary: Currently unused, Holds a JSON dictionary, makes it easier to pass a JSON dictionary to where it is 
+ needed.
+
+ChampionWinrateStatistics: A class that has two arrays. One array holds the amount of games that a certain champion 
+ was seen in. The other array holds the amount of wins that the champion has. By knowing these two number we can 
+ calculate the champion's winrate.
+ 
+PlayerDictionary: A class that holds all of the summoner IDs that we have explored. It holds an array that tells us
+ the most recent match that the summoner has competed in (ranked, or normal summoners rift). By knowing the most recent
+ match we will avoid double counting games if we encounter the summoner again.
+
+SummonerIDsToExplore: A class that holds an array of summoner IDs. When prompted, it will either put a summoner ID that
+ we encountered into the array, or give us a summoner ID for us to explore for match history information.
+
+"""
+##########################################################################################################################
+
 
 # a class that holds a JSON dictionary, since python doesn't have pointers we use a class whose value we can manipulate from different locations and have persistent data changes
+
 class JSONDictionary(object):
+	
 	def __init__(self, Dict):
+		
 		self.JSONDict = Dict
 
 	def ReturnDictionary(self):
+		
 		return JSONDict
 
 	# prints this class' self.JSONDict
 	def PrintDictionary(self, Dictionary):
+		
 		print json.dumps(self.JSONDict, indent = 2, sort_keys=False)
 
 	# prints any JSON passed as an argumentID
 	def PrintOtherDictionary(self, Dictionary):
+		
 		print json.dumps(Dictionary, indent = 2, sort_keys=False)
 
-# this class holds statistics on number of games each champion has played and won. By using these two numbers we can calculate winrate statistics
+
+# This class holds statistics on number of games each champion has played and won. By using these two numbers we can calculate winrate statistics
+
 class ChampionWinrateStatistics(object):
+	
 	# each champion has a unique ID. However they are not 1-(current # of champs), instead they are non sequential. However no champ ID goes above 300, so we initialize arrays with the size of 300.
 	ChampionIDDictionary = [0]*500
 	ChampionWins = [0]*500
 	ChampionGames = [0]*500
 
 	def DoesChampionExist(self, ChampionID):
+		
 		if (self.ChampionIDDictionary[ChampionID]):
 			return True
 		else:
 			return False
 
 	def ReturnGames(self, ChampionID):
+		
 		if (self.ChampionGames(ChampionID)):
 			return self,ChampionGames[ChampionID]
 		else:
 			return 0
 
 	def ReturnWins(self, ChampionID):
+		
 		if (self.ChampionWins[ChampionID]):
 			return self.ChampionWins[ChampionID]
 		else:
 			return 0
 
 	def AddChampion(self, ChampionID):
+		
 		self.ChampionIDDictionary[ChampionID] = ChampionID
 		self.ChampionWins[ChampionID] = 0
 		self.ChampionGames[ChampionID] = 0
 
 	def IncrementGames(self, ChampionID):
+		
 		if (self.ChampionIDDictionary[ChampionID]):
 			if (self.ChampionGames[ChampionID]):
 			# if champion has been seen before, increment number of games by one
@@ -58,6 +94,7 @@ class ChampionWinrateStatistics(object):
 				print ChampionInfoDict['name'], " has ", games, "games, and ", wins, " wins. Leaving him with a ", wins/games, " percent win chance"
 
 	def IncrementWins(self, ChampionID):
+		
 		if (self.ChampionIDDictionary[ChampionID]):
 			if (self.ChampionWins[ChampionID]):
 			# if champion has been seen before, increment number of wins by one
@@ -71,11 +108,13 @@ class ChampionWinrateStatistics(object):
 # by keeping track of their last game id that we have seen, we will not take duplicate game information.
 
 class PlayerDictionary(object):
+	
 	PlayerIDArray = [None]*90000000 #how to store 60+ million is a problem. computer cannot handle 600 million when initializing this array
 	PlayerLastGameDictionary = [None]*90000000
 
 	# method to determine if we have encountered this player before
 	def DoesPlayerExist(self, SummonerID):
+		
 		# if we have encountered player before, return true, else return false
 		if (self.PlayerIDArray[SummonerID]):
 			return True
@@ -86,6 +125,7 @@ class PlayerDictionary(object):
 	# if a player is not in hash, store them and record most recent game
 	# by recording most recent game ID, if we encounter a summoner again we will not double count games we have seen before for our statistics.
 	def UpdatePlayer(self, SummonerID, MostRecentGameID):
+		
 		if (self.PlayerIDArray[SummonerID]):
 			# summoner seen before
 			print ("summoner seen before")
@@ -103,19 +143,23 @@ class PlayerDictionary(object):
 
 	# return players most recent game IDa
 	def GetMostRecentMatchID(self, SummonerID):
+		
 		# if player exists (we have encountered him before, then return his most recent game id
 		if (self.PlayerIDArray[SummonerID]):
 			return self.PlayerLastGameDictionary[SummonerID]
 
 
 class SummonerIDsToExplore(object):
+	
 	ArrayTooBig = False # This bool is so that we stop adding player IDs after a certain array length is met.
 	PlayerIDArray = [] # start empty, add IDs as the program progresses
 
 	def __init__(self):
+		
 		self.ArrayTooBig = False
 
 	def AddIDToExplore(self, SummonerID):
+		
 		ArraySize = len(self.PlayerIDArray)
 		# check to see if ID is in array, if yes, don't add it
 		# length of array capped at 1000 so that it does not expand infinitely
@@ -137,5 +181,6 @@ class SummonerIDsToExplore(object):
 			#print "summoner id in array already, ignoring"
 
 	def GetSummonerIDToExplore(self):
+		
 		SummonerID = self.PlayerIDArray.pop()
 		return SummonerID
